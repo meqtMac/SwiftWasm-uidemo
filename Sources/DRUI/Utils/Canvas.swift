6f3dfeceffc85201a0611cc842ba8c604e34e499
meqt-imac1 <meqt@foxmail.com>
2024-03-15 18:01:37 +0800

    Add Canvas

--- Sources/DRUI/Utils/Canvas.swift
+++ Sources/DRUI/Utils/Canvas.swift
@@ -1,8 +1,23 @@
 //
-//  File.swift
+//  Canvas.swift
 //  
 //
 //  Created by 蒋艺 on 2024/3/15.
 //
 
 import Foundation
+import DOM
+//import WebAPIBase
+
+struct Canvas {
+    internal let element: HTMLCanvasElement
+    var context: CanvasRenderingContext2D {
+        element.getContext(CanvasRenderingContext2D.self)!
+    }
+    
+    init(width: UInt32, height: UInt32) {
+        element = HTMLCanvasElement(from: globalThis.document.createElement(localName: "canvas"))!
+        element.width = width
+        element.height = height
+    }
+}
